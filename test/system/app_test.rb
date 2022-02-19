7c9a30feb04e1bdc40375a7aecb196da6531aace
Yves Siegrist <Elektron1c97@gmail.com>
2022-02-19 23:44:55 +0100

    Make table sortable
    Thanks to https://github.com/tofsjonas/sortable vanilla sortable javascript.

--- test/system/app_test.rb
+++ test/system/app_test.rb
@@ -1,6 +1,11 @@
 require "application_system_test_case"
 
 class AppTest < ApplicationSystemTestCase
+  YORICK = ["Yorick", "1", "0%", "5,00", "9,3"]
+  ZILEAN = ["Zilean", "1", "100%", "Perfekt", "1,2"]
+  ZOE = ["Zoe", "2", "50%", "3,56", "8,4"]
+  ZYRA = ["Zyra", "1", "100%", "6,00", "2,1"]
+
   test "test" do
     visit '/'
 
@@ -10,10 +15,34 @@ class AppTest < ApplicationSystemTestCase
     assert_equal "60%", find("#winrate").text
 
     assert_equal [
-      ["Zoe", "2", "50%", "3.56", "8.4"],
-      ["Yorick", "1", "0%", "5.0", "9.3"],
-      ["Zilean", "1", "100%", "Perfekt", "1.2"],
-      ["Zyra", "1", "100%", "6.0", "2.1"]
+      ZOE,
+      YORICK,
+      ZILEAN,
+      ZYRA
+    ], champ_stats
+
+    find("#sort-by-champion").click()
+    assert_equal [
+      ZYRA,
+      ZOE,
+      ZILEAN,
+      YORICK
+    ], champ_stats
+    find("#sort-by-champion").click()
+    assert_equal [
+      YORICK,
+      ZILEAN,
+      ZOE,
+      ZYRA
+    ], champ_stats
+
+
+    find("#sort-by-winrate").click()
+    assert_equal [
+      ZILEAN,
+      ZYRA,
+      ZOE,
+      YORICK
     ], champ_stats
   end
 
