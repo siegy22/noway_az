63b07550ce596286ce27acff34c0c1890ddb86fe
Yves Siegrist <Elektron1c97@gmail.com>
2022-02-24 18:00:45 +0100

    Make puuid configurable

--- config/application.rb
+++ config/application.rb
@@ -16,9 +16,9 @@ module NowayZA
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.noway_puuid = "IslvKjK8xB5PEnrJctqRCLrKW7DDLCiYUUSp32_R-M5pvKlyMMdI-KaqhrpbJZf5GKzIammxPBES0g"
+    config.time_zone = "Europe/Berlin"
+    config.noway_puuid = ENV.fetch("RIOT_API_PUUID", "IslvKjK8xB5PEnrJctqRCLrKW7DDLCiYUUSp32_R-M5pvKlyMMdI-KaqhrpbJZf5GKzIammxPBES0g")
     config.api_key = ENV["RIOT_API_KEY"]
   end
 end
